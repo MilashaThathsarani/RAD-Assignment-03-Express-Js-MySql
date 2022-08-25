21e966fc7ac9fd9270a2926468461d49af8a9fed
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-08-25 23:05:21 +0530

    Sucessfully connected mysql

--- routes/customer.js
+++ routes/customer.js
@@ -3,4 +3,23 @@ const router = express.Router();
 const mysql = require('mysql')
 const db = require('../configs/db.config')
 
+
+const connection = mysql.createConnection(db._database)
+
+connection.connect(function (error) {
+    if (error) {
+        console.log(error);
+    }else {
+        console.log("Connected to the MySQL Server")
+        var userTableQuery = "CREATE TABLE IF NOT EXISTS fake-store-express (id VARCHAR(255) PRIMARY KEY , name VARCHAR (255),address VARCHAR (255), contact VARCHAR (255))"
+        connection.query(userTableQuery, function (error,result) {
+            if (error) throw error;
+            //console.log(result);
+            if (result.warningCount === 0){
+                console.log("fake-store-express table Created");
+            }
+        })
+    }
+})
+
 module.exports = router
