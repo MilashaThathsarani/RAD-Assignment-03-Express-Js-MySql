58edf9102efd294bcd3b00ed7f341e8acd84c136
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-08-26 12:06:43 +0530

    Order table created

--- routes/order.js
+++ routes/order.js
@@ -3,4 +3,21 @@ const router = express.Router();
 const mysql = require('mysql')
 const db = require('../configs/db.config')
 
+const connection = mysql.createConnection(db._database)
+
+connection.connect(function (error) {
+    if (error) {
+        console.log(error);
+    }else {
+        console.log("Connected to the MySQL Server")
+        var userTableQuery = "CREATE TABLE IF NOT EXISTS orders (orderId VARCHAR(255) PRIMARY KEY , date DATE, customerId VARCHAR(255))"
+        connection.query(userTableQuery, function (error,result) {
+            if (error) throw error;
+            //console.log(result);
+            if (result.warningCount === 0){
+                console.log("order table Created");
+            }
+        })
+    }
+})
 module.exports = router
