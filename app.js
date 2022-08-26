55c83003360f29dffe6d6c7fe68e56c14f8837b7
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-08-26 12:35:36 +0530

    Added orderDetails.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const customer = require('./routes/customer.js')
 const item = require('./routes/item')
 const order = require('./routes/order')
+const orderDetails = require('./routes/orderDetails')
 
 const app = express();
 const port = 4000;
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use('/customer',customer)
 app.use('/item',item)
 app.use('/order',order)
+app.use('/orderDetails',orderDetails)
 
 
 app.listen(port,(req,res) => {
